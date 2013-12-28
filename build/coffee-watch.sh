a2bcfdc6034920cffa0c8ce1777b98ab0e4c8f49
Sam <sam@1485048.no-reply.drupal.org>
2013-12-29 01:13:52 +0800

    Comment on build script

--- build/coffee-watch.sh
+++ build/coffee-watch.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #
-#@todo: convert to a cakefile.
+# todo: convert to a cakefile or something better suited to building JS. Would
+# be nice to include a packed jsmin version of the code for inclusion.
 
 coffee --watch --output bin/ src/
